55262630f64cff54fc2c74bc7de054244750443b
Tim Perkins <perkins@adobe.com>
2016-10-02 13:09:25 -0600

    Add some cool page transitions

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -6,25 +6,44 @@ import {
 	browserHistory,
 	IndexRedirect
 } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import Shell from 'components/Shell';
 import GamePage from 'pages/GamePage';
 import CreateUserPage from 'pages/CreateUserPage';
 import GameListPage from 'pages/GameListPage';
 import CreateGamePage from 'pages/CreateGamePage';
 
+function AppPages({children, location}) {
+	const transitionTime = 400;
+	return (
+		<ReactCSSTransitionGroup
+			component="div"
+			transitionName="page"
+			className="page"
+			transitionEnterTimeout={transitionTime}
+			transitionLeaveTimeout={transitionTime}>
+			{React.cloneElement(children, {
+				key: location.pathname
+			})}
+		</ReactCSSTransitionGroup>
+	);
+}
+
 export default class App extends React.Component {
 	render() {
 		return (
-			<Router history={browserHistory}>
-    		<Route path="/" component={Shell}>
-    			<IndexRedirect to="/create-user" />
-    			<Route path="/create-user" component={CreateUserPage} />
-    			<Route path="/game" component={GamePage} />
-    			<Route path="/game/:id" component={GamePage} />
-    			<Route path="/game-list" component={GameListPage} />
-    			<Route path="/create-game" component={CreateGamePage} />
-    		</Route>
-    	</Router>
+			<Shell>
+				<Router history={browserHistory}>
+					<Route path="/" component={AppPages}>
+						<IndexRedirect to="/create-user" />
+						<Route path="/create-user" component={CreateUserPage} />
+						<Route path="/game" component={GamePage} />
+						<Route path="/game-list" component={GameListPage} />
+						<Route path="/game/:id" component={GamePage} />
+						<Route path="/create-game" component={CreateGamePage} />
+					</Route>
+				</Router>
+			</Shell>
 		);
 	}
 }
--- client/app/components/Shell.jsx
+++ client/app/components/Shell.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 export default class Shell extends React.Component {
 	render() {
 		return (
-			<div>
-				<div className="page">
-					{this.props.children}
-				</div>
+			<div className="shell">
+				{this.props.children}
 			</div>
 		);
 	}
