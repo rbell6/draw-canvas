ce7edf501d40f906154c43d05c438f6ab3a894c4
Tim Perkins <perkins@adobe.com>
2016-11-27 08:37:12 -0700

    Make the logo clickable

--- client/app/components/Shell.jsx
+++ client/app/components/Shell.jsx
@@ -1,12 +1,19 @@
 import styles from '../less/shell.less';
 import React from 'react';
+import {
+	browserHistory
+} from 'react-router';
 
 export default class Shell extends React.Component {
+	onLogoClick() {
+		browserHistory.push('/game-list');
+	}
+
 	render() {
 		return (
 			<div className="shell">
-				<img src="/static/img/logo.png" className="game-logo-small" />
 				{this.props.children}
+				<img src="/static/img/logo.png" className="game-logo-small" onClick={() => this.onLogoClick()} />
 			</div>
 		);
 	}
