9f79e5fef97570b3fd179c6e206bc8adbad4a51d
Tim Perkins <perkins@adobe.com>
2016-11-28 20:49:43 -0700

    Force SSL

--- server/server.js
+++ server/server.js
@@ -5,12 +5,23 @@ let socketIO = require('socket.io');
 let path = require('path');
 
 let PORT = process.env.PORT || 3007;
+let ENV = process.env.NODE_ENV || 'development';
 let buildDir  = path.resolve(__dirname, './public');
 
 let app = express();
 let http = require('http');
 let server = http.createServer(app);
 let bodyParser = require('body-parser');
+
+let forceSSL = (req, res, next) => {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		return res.redirect(['https://', req.get('Host'), req.url].join(''));
+	}
+	return next();
+};
+if (ENV === 'production') {
+	app.use(forceSSL);
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
